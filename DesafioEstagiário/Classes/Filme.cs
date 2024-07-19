2eef252255a2e5e076baee49f88f1835046a834c
mauricio <mauricioquintella27@gmail.com>
2024-07-19 09:12:10 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioEstagiário/Classes/Filme.cs
@@ -0,0 +1,13 @@
+﻿namespace DesafioEstagiário.Classes
+{
+    public class Filme
+    {
+        public string? Nome { get; set; }
+        public int FaixaEtaria { get; set; }
+        public int Duracao { get; set; }
+        public string? Direcao { get; set; }
+        public string? AnoDeLancamento { get; set; }
+        public string? Roteiristas { get; set; }
+        public string? Artistas { get; set; }
+    }
+}
---
+++ DesafioEstagiário/Controllers/FilmeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioEstagiário.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmeController : ControllerBase
+    {
+        [HttpGet]
+        public string GetFilmes() 
+        {
+            return null;
+        }
+    }
+}
